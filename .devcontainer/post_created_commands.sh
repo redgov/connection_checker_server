fac95b7c60ef490281b85a12bbe9b79bb3deeeac
redgov <governred@gmail.com>
2020-08-30 09:49:10 +0000

    move codes only using at development

---
+++ .devcontainer/post_created_commands.sh
@@ -0,0 +1,7 @@
+# add os packages
+apt-get update
+apt-get install git-flow
+
+# install poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+poetry install
